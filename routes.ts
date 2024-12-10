6c125c0c3323be1f116affcea209b24ad96d6ef5
Shirloin <riccardo@binus.ac.id>
2024-12-10 07:37:19 +0700

    fix: route

--- routes.ts
+++ routes.ts
@@ -4,11 +4,11 @@ export const publicRoutes = [""];
 
 export const apiAuthPrefix = "/api/auth";
 
-export const protectedRoutes = ["/company/list",];
+export const protectedRoutes = ["/company/list", "/account"];
 
 export const adminRoutes = ["/company/new", "/account/new", "/student/list"];
 
-export const studentRoutes = ["/cv/new", "/account"];
+export const studentRoutes = ["/cv/new",];
 
 export const roleRoutes = {
   "/": ["student", "admin"],
